4215d199fd9340452479da85700c40fd413d40e8
Julian Hartmer <j.hartmer@googlemail.com>
2021-08-16 14:46:26 +0200

    Undo failed test. The CI showed that the tests failed, so I consider the CI to be working now :)

--- BngineTests/test.cpp
+++ BngineTests/test.cpp
@@ -88,10 +88,6 @@ TEST(Mat, V3fmethods) {
 
 }
 
-TEST(Mat, shouldFail) {
-	EXPECT_TRUE(false);
-}
-
 int main(int argc, char** argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
