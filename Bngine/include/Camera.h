a467aa82ec8cb6d3bfdee5b583ff35c95a252358
Benno Kutschank <benku@live.de>
2021-09-01 13:33:40 +0200

    Added getter and setter methods for camera properties.

--- Bngine/include/Camera.h
+++ Bngine/include/Camera.h
@@ -11,6 +11,16 @@ namespace Bngine {
 		Camera(void);
 		Camera(float aspect_ratio, float fov, float clipping_near, float clipping_far);
 
+		float aspect_ratio(void);
+		float fov(void);
+		float clipping_near(void);
+		float clipping_far(void);
+
+		void set_aspect_ratio(float aspect_ratio);
+		void set_fov(float fov);
+		void set_clipping_near(float clipping_near);
+		void set_clipping_far(float clipping_far);
+
 	private:
 		float _aspect_ratio;
 		float _fov;
--- Bngine/src/Camera.cpp
+++ Bngine/src/Camera.cpp
@@ -15,7 +15,58 @@ namespace Bngine {
 		projection.m[0][0] = aspect_ratio * 1/tanf(fov_rad);
 		projection.m[1][1] = 1 / tanf(fov_rad / 2);
 		projection.m[2][2] = clipping_far / q;
-		projection.m[3][2] = projection.m[2][2] * -clipping_near;
+		projection.m[3][2] = projection.m[2][2] * - clipping_near;
 		projection.m[2][3] = 1;
+	}
+
+	float Camera::aspect_ratio(void)
+	{
+		return _aspect_ratio;
+	}
+
+	float Camera::fov(void)
+	{
+		return _fov;
+	}
+
+	float Camera::clipping_near(void)
+	{
+		return _clipping_near;
+	}
+
+	float Camera::clipping_far(void)
+	{
+		return _clipping_far;
+	}
+
+	void Camera::set_aspect_ratio(float aspect_ratio)
+	{
+		_aspect_ratio = aspect_ratio;
+		float fov_rad = _fov / 360 * 2 * M_PI;
+		projection.m[0][0] = aspect_ratio * 1 / tanf(fov_rad);
+	}
+
+	void Camera::set_fov(float fov)
+	{
+		_fov = fov;
+		float fov_rad = fov / 360 * 2 * M_PI;
+		projection.m[0][0] = _aspect_ratio * 1 / tanf(fov_rad);
+		projection.m[1][1] = 1 / tanf(fov_rad / 2);
+	}
+
+	void Camera::set_clipping_near(float clipping_near)
+	{
+		_clipping_near = clipping_near;
+		float q = _clipping_far - _clipping_near;
+		projection.m[2][2] = _clipping_far / q;
+		projection.m[3][2] = projection.m[2][2] * - _clipping_near;
+	}
+
+	void Camera::set_clipping_far(float clipping_far)
+	{
+		_clipping_far = clipping_far;
+		float q = _clipping_far - _clipping_near;
+		projection.m[2][2] = _clipping_far / q;
+		projection.m[3][2] = projection.m[2][2] * - _clipping_near;
 	}
 }
