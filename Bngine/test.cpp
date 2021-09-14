0a87a3b3411a862607e199b854fa056b43574a48
Benno Kutschank <benku@live.de>
2021-09-14 15:55:07 +0200

    Added first little try at movement of the camera which revealed an unknown bug with drawing, likely related to border issues.

--- Bngine/test.cpp
+++ Bngine/test.cpp
@@ -18,8 +18,6 @@ int main()
 		std::cout << SDL_GetError();
 	}
 	Camera cam = Camera(x, y, V3f(0, 0, 0), 90, 1, 300);
-	cam.set_direction(V3f(1, 0, 0));
-	cam.set_rotation(V3f(0, -40, 0));
 	Cube c = Cube(9, 3, 8, 2);
 	Pyramid p = Pyramid(-9, 3, 20, 2, 3);
 	Icosahedron i = Icosahedron(2, 1, 10, 1);
@@ -27,30 +25,42 @@ int main()
 
 	SDL_SetRenderDrawColor(r, 0, 0, 0, 0xFF);
 	SDL_RenderClear(r);
+	
 	SDL_SetRenderDrawColor(r, 0xFF, 0xFF, 0xFF, 0xFF);
 
-	std::vector<Tri2D> projected = c.project(cam);
-	for (Tri2D t : projected) {
-		SDL_RenderDrawLine(r, t.tris[0].x, t.tris[0].y, t.tris[1].x, t.tris[1].y);
-		SDL_RenderDrawLine(r, t.tris[0].x, t.tris[0].y, t.tris[2].x, t.tris[2].y);
-		SDL_RenderDrawLine(r, t.tris[1].x, t.tris[1].y, t.tris[2].x, t.tris[2].y);
-	}
-	projected = p.project(cam);
-	for (Tri2D t : projected) {
-		SDL_RenderDrawLine(r, t.tris[0].x, t.tris[0].y, t.tris[1].x, t.tris[1].y);
-		SDL_RenderDrawLine(r, t.tris[0].x, t.tris[0].y, t.tris[2].x, t.tris[2].y);
-		SDL_RenderDrawLine(r, t.tris[1].x, t.tris[1].y, t.tris[2].x, t.tris[2].y);
-	}
-	projected = i.project(cam);
-	for (Tri2D t : projected) {
-		SDL_RenderDrawLine(r, t.tris[0].x, t.tris[0].y, t.tris[1].x, t.tris[1].y);
-		SDL_RenderDrawLine(r, t.tris[0].x, t.tris[0].y, t.tris[2].x, t.tris[2].y);
-		SDL_RenderDrawLine(r, t.tris[1].x, t.tris[1].y, t.tris[2].x, t.tris[2].y);
+	for (float j = 0;j < 4000;j += 0.2)
+	{
+		SDL_SetRenderDrawColor(r, 0, 0, 0, 0xFF);
+		SDL_RenderClear(r);
+		SDL_SetRenderDrawColor(r, 0xFF, 0xFF, 0xFF, 0xFF);
+		std::vector<Tri2D> projected = c.project(cam);
+		for (Tri2D t : projected) {
+			SDL_RenderDrawLine(r, t.tris[0].x, t.tris[0].y, t.tris[1].x, t.tris[1].y);
+			SDL_RenderDrawLine(r, t.tris[0].x, t.tris[0].y, t.tris[2].x, t.tris[2].y);
+			SDL_RenderDrawLine(r, t.tris[1].x, t.tris[1].y, t.tris[2].x, t.tris[2].y);
+		}
+		projected = p.project(cam);
+		for (Tri2D t : projected) {
+			SDL_RenderDrawLine(r, t.tris[0].x, t.tris[0].y, t.tris[1].x, t.tris[1].y);
+			SDL_RenderDrawLine(r, t.tris[0].x, t.tris[0].y, t.tris[2].x, t.tris[2].y);
+			SDL_RenderDrawLine(r, t.tris[1].x, t.tris[1].y, t.tris[2].x, t.tris[2].y);
+		}
+		projected = i.project(cam);
+		for (Tri2D t : projected) {
+			SDL_RenderDrawLine(r, t.tris[0].x, t.tris[0].y, t.tris[1].x, t.tris[1].y);
+			SDL_RenderDrawLine(r, t.tris[0].x, t.tris[0].y, t.tris[2].x, t.tris[2].y);
+			SDL_RenderDrawLine(r, t.tris[1].x, t.tris[1].y, t.tris[2].x, t.tris[2].y);
+		}
+		
+		SDL_RenderPresent(r);
+		cam.set_rotation(V3f(0, j * 2, j));
+		SDL_Delay(10);
+		
 	}
 
-	SDL_RenderPresent(r);
+	
 
-	SDL_Delay(10000);
+	
 
 	return 1;
 }
